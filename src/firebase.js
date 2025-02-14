47d8b3ab1262795e9d0dadb31513f361c3172d6d
R4nd0mg4y <tuandoviet2002@gmail.com>
2025-02-14 22:40:05 +0700

    add new features

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
-import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Lấy thông số từ môi trường
 const firebaseConfig = {
-  apiKey: "AIzaSyC3pLaLTm16PD2U_GN3oiey0rm_Civvzo4",
-  authDomain: "coding-website-59da5.firebaseapp.com",
-  projectId: "coding-website-59da5",
-  storageBucket: "coding-website-59da5.firebasestorage.app",
-  messagingSenderId: "378151871504",
-  appId: "1:378151871504:web:682829d96be887b7541695",
-  measurementId: "G-Z1HLXXCE1W"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 export default app;
